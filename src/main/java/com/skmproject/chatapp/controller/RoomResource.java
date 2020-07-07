6acbcc7496f4f32d8da2b7d45f41b7b0fbbac8f5
Sujan Kumar Mitra <mitrakumarsujan@gmail.com>
2020-07-07 12:50:56 +0530

    added room resource

---
+++ src/main/java/com/skmproject/chatapp/controller/RoomResource.java
@@ -0,0 +1,46 @@
+package com.skmproject.chatapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skmproject.chatapp.model.CreateRoom;
+import com.skmproject.chatapp.model.Room;
+import com.skmproject.chatapp.service.RoomService;
+
+/**
+ * @author Sujan Kumar Mitra
+ * @since 2020-07-07
+ */
+@RestController
+@RequestMapping("/api")
+public class RoomResource {
+
+	@Autowired
+	private RoomService roomService;
+
+	@PostMapping("/room")
+	public ResponseEntity<Room> createRoom(@RequestBody CreateRoom payload) {
+		Room room = roomService.createRoom(payload);
+		return ResponseEntity
+				.status(HttpStatus.CREATED)
+				.body(room);
+	}
+
+	@GetMapping("/room/{roomId}")
+	public ResponseEntity<Void> existsRoom(@PathVariable("roomId") String roomId) {
+		boolean result = roomService.existsRoom(roomId);
+		if(result) {
+			return ResponseEntity.status(HttpStatus.FOUND).build();
+		} else {
+			return ResponseEntity.notFound().build();			
+		}
+	}
+
+}
