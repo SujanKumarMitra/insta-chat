440d0c78aa151c571af42abbc575ad75423ec9c0
Sujan Kumar Mitra <mitrakumarsujan@gmail.com>
2020-07-07 13:13:17 +0530

    Revert "removed main mathod"
    This reverts commit d6ba02c2fcbd762048390d912bbed0aba5f74c71.

--- src/main/java/com/skmproject/chatapp/util/SimpleRandomGenerator.java
+++ src/main/java/com/skmproject/chatapp/util/SimpleRandomGenerator.java
@@ -34,4 +34,8 @@ public class SimpleRandomGenerator implements RandomGenerator {
 		return str;
 	}
 
+	public static void main(String[] args) {
+		System.out.println(new SimpleRandomGenerator().generateRandom());
+	}
+
 }
