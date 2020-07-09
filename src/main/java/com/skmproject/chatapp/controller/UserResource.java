465206450d90cec6a483c1d75a2d81951d2cfdda
Sujan Kumar Mitra <mitrakumarsujan@gmail.com>
2020-07-09 11:21:13 +0530

    added user resource to fetch session id

---
+++ src/main/java/com/skmproject/chatapp/controller/UserResource.java
@@ -0,0 +1,34 @@
+package com.skmproject.chatapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.skmproject.chatapp.model.User;
+import com.skmproject.chatapp.service.OnlineTrackerService;
+
+/**
+ * @author Sujan Kumar Mitra
+ * @since 2020-07-09
+ */
+@Controller
+@RequestMapping("/api")
+public class UserResource {
+	
+	@Autowired
+	OnlineTrackerService trackerService;
+	
+	@GetMapping("/user/{username}")
+	public ResponseEntity<User> getUser(@PathVariable("username") String username) {
+		User user = trackerService.getUser(username);
+		if(user == null) {
+			return ResponseEntity.notFound().build();
+		} else {
+			return ResponseEntity.ok(user);
+		}
+	}
+	
+}
