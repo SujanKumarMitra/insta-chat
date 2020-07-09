ee5da5a015cfc7f4630a1d49824b59bfae86e373
Sujan Kumar Mitra <mitrakumarsujan@gmail.com>
2020-07-09 11:22:54 +0530

    added method to get list of connected users in a room

--- src/main/java/com/skmproject/chatapp/controller/RoomResource.java
+++ src/main/java/com/skmproject/chatapp/controller/RoomResource.java
@@ -1,5 +1,8 @@
 package com.skmproject.chatapp.controller;
 
+import java.util.Collection;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.skmproject.chatapp.model.CreateRoom;
 import com.skmproject.chatapp.model.Room;
+import com.skmproject.chatapp.model.User;
+import com.skmproject.chatapp.service.OnlineTrackerService;
 import com.skmproject.chatapp.service.RoomService;
 
 /**
@@ -24,6 +29,9 @@ public class RoomResource {
 
 	@Autowired
 	private RoomService roomService;
+	
+	@Autowired
+	private OnlineTrackerService usersTrackerService;
 
 	@PostMapping("/room")
 	public ResponseEntity<Room> createRoom(@RequestBody CreateRoom payload) {
@@ -37,6 +45,18 @@ public class RoomResource {
 		return ResponseEntity
 				.status(HttpStatus.FOUND)
 				.body(room);
+	}
+	
+	@GetMapping("/room/{roomId}/user")
+	public ResponseEntity<Collection<User>> getConnectedUsers(@PathVariable("roomId") String roomId) {
+		Set<User> onlineUsers = usersTrackerService.getOnlineUsers(roomId);
+		if(onlineUsers == null || onlineUsers.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		} else {
+			return ResponseEntity
+					.status(HttpStatus.FOUND)
+					.body(onlineUsers);
+		}
 	}
 
 }
