1bae40464a766d8b30c2b3624847c71253d4eabd
Sujan Kumar Mitra <mitrakumarsujan@gmail.com>
2020-07-09 11:23:52 +0530

    added destination constants

---
+++ src/main/java/com/skmproject/chatapp/util/DestinationConstants.java
@@ -0,0 +1,12 @@
+package com.skmproject.chatapp.util;
+
+/**
+ * @author Sujan Kumar Mitra
+ * @since 2020-07-08
+ */
+public interface DestinationConstants {
+	
+	final String MESSAGE_DESTINATION = "/messages/";
+	
+	final String EVENT_DESTINATION = "/events/";
+}
